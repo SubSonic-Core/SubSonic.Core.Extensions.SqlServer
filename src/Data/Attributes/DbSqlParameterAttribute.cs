1503656741de793a824737330d87f73d2ad985e7
Kenneth Carter <kccarter32@gmail.com>
2020-07-15 20:02:37 -0600

    4.2.0-beta.1 check in

---
+++ src/Data/Attributes/DbSqlParameterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using SubSonic;
+using System.Data;
+
+namespace SubSonic.Extensions.SqlServer
+{
+    public class DbSqlParameterAttribute
+        : DbParameterAttribute
+    {
+        public DbSqlParameterAttribute(string name)
+            : base(name)
+        {
+        }
+
+        private SqlDbType sqlDbType;
+
+        public SqlDbType SqlDbType
+        {
+            get
+            {
+                return sqlDbType;
+            }
+            set
+            {
+                sqlDbType = value;
+
+                DbType = TypeConvertor.ToDbType(value);
+            }
+        }
+    }
+}
---
+++ src/ServerSqlQueryProvider/SqlServerSqlQueryProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SubSonic.Extensions.SqlServer
+{
+    using SqlGenerator;
+
+    public class SqlServerQueryProvider
+        : SqlQueryProvider
+    {
+        public SqlServerQueryProvider()
+            : base(CreateSqlContext<AnsiSqlFragment, SqlServerSqlMethods>())
+        {
+
+        }
+
+        public override string ClientName => typeof(SqlClientFactory).FullName;
+    }
+}
